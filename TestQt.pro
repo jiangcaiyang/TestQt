bf8acee3bc9127ce9676ffa7f635c1640f66947e
jiangcaiyang <jiangcaiyang123@163.com>
2013-07-19 14:05:19 +0800

    Ver. 1.1
    This is the second version containing the whole test case.

--- TestQt.pro
+++ TestQt.pro
@@ -1,1 +1,4 @@
 QT += core gui widgets
+
+SOURCES += \
+    main.cpp
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+#include <QTranslator>
+
+int main( int argc, char** argv )
+{
+    QApplication app( argc, argv );
+
+
+    return app.exec( );
+}
